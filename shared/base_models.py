e53403c9a0644b38b950c0007d46234a2f41c8a0
tim-hub <tim-hub@users.noreply.github.com>
2024-08-27 21:30:57 +1200

    auto create uuid

--- shared/base_models.py
+++ shared/base_models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    uuid = models.UUIDField(primary_key=True, editable=False)
+    uuid = models.UUIDField(primary_key=True, editable=False, auto_created=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
