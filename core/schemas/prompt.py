2d480bee9a62db6044d2ff90e0c1c6fb36af5651
tim-hub <tim-hub@users.noreply.github.com>
2024-08-29 15:58:08 +1200

    use ninja pa=ydantec schema for validation

--- core/schemas/prompt.py
+++ core/schemas/prompt.py
@@ -1,9 +1,16 @@
+from typing import Optional
+
 from ninja import ModelSchema
+from pydantic import constr
 
 from core.models import Prompt
 
 
 class PromptSchema(ModelSchema):
+    unique_key: constr(max_length=512, min_length=4)
+    description: str
+    enabled: Optional[bool] = True
+
     class Meta:
         model = Prompt
         fields = (
@@ -11,3 +18,8 @@ class PromptSchema(ModelSchema):
             'description',
             'enabled',
         )
+
+
+PromptOutSchema = PromptSchema
+
+PromptInSchema = PromptSchema
