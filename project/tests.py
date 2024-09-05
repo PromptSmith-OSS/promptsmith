006bf0c8b1a7135cf4ee9ebe78868ca225578960
tim-hub <tim-hub@users.noreply.github.com>
2024-09-05 23:19:15 +1200

    add token gen utils

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .utils import generate_token, decode_token, generate_secure_token
+from uuid import UUID
 
-# Create your tests here.
+
+class TestUtils(TestCase):
+
+    def test_generate_token(self):
+        token = generate_token('user_uuid', 'project_uuid')
+        self.assertTrue(token)
+
+
+    def test_decode_token(self):
+        token = generate_token('user_uuid', 'project_uuid')
+        decoded = decode_token(token)
+        self.assertEqual(decoded['user_uuid'], 'user_uuid')
+        self.assertEqual(decoded['project_uuid'], 'project_uuid')
+
+    def test_generate_secure_token(self):
+        token = generate_secure_token()
+        self.assertTrue(token)
+        self.assertEqual(len(token), 64)
---
+++ project/utils.py
@@ -0,0 +1,42 @@
+import jwt
+import datetime
+from uuid import UUID
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+import secrets
+
+jwt_secret = os.getenv('JWT_SECRET')
+
+
+def generate_token(user_uuid: UUID | str, project_uuid: UUID | str, days: int = 365) -> str:
+    """
+    Generate a jwt token valid for 365 days
+    """
+    payload = {
+        'user_uuid': str(user_uuid),
+        'project_uuid': str(project_uuid),
+        'iat': datetime.datetime.now(datetime.timezone.utc),
+        'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=days)
+    }
+    return jwt.encode(payload, jwt_secret, algorithm='HS256')
+
+
+def decode_token(token: str) -> dict:
+    """
+    Decode a token
+    """
+    return jwt.decode(token, jwt_secret, algorithms=['HS256'])
+
+
+def generate_secure_token(desired_token_length=64) -> str:
+    """
+    Generate a secure token of length 64
+    :param desired_token_length:
+    :return:
+    """
+    bytes_length = int(desired_token_length / 4 * 3)
+    token = secrets.token_urlsafe(bytes_length)  # return a URL-safe text string
+    return token
