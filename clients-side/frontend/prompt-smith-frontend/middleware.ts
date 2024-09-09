c8eadb0d4dccc49de6e5200c9ec3a8ee660e5aeb
tim-hub <tim-hub@users.noreply.github.com>
2024-09-09 22:57:14 +1200

    update middleware for headers auth

--- clients-side/frontend/prompt-smith-frontend/middleware.ts
+++ clients-side/frontend/prompt-smith-frontend/middleware.ts
@@ -6,7 +6,53 @@ import {cookies} from 'next/headers'
 const protectedRoutes = ['/dashboard', '/logout']
 const publicRoutes = ['/login', '/signup', '/', 'simple-login']
 
-export default async function middleware(req: NextRequest) {
+const allowedOrigins = ['https://acme.com', 'https://my-app.org']
+
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+
+const setHeaders = async (request: NextRequest) => {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS'
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && {'Access-Control-Allow-Origin': origin}),
+      ...corsOptions,
+    }
+    return NextResponse.json({}, {headers: preflightHeaders})
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next()
+
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value)
+  })
+
+  const cookie = cookies().get('session')?.value
+  const session = await decrypt(cookie)
+  if (session?.bearerToken) {
+    response.headers.set('Authorization', `Bearer ${session.bearerToken}`)
+  }
+
+  return response
+}
+
+
+const checkRedirect = async (req: NextRequest) => {
+
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname
   const isProtectedRoute = protectedRoutes.includes(path)
@@ -33,7 +79,18 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next()
 }
 
+
+export default async function middleware(req: NextRequest) {
+
+
+  if (req.nextUrl.pathname.startsWith('/api')) {
+    return setHeaders(req)
+  } else {
+    return checkRedirect(req)
+  }
+}
+
 // Routes Middleware should not run on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!_next/static|_next/image|.*\\.png$).*)'],
 }
