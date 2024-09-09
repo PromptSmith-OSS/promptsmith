2f9687f6fb9a4f7d2039022b50663ebbd8d84a55
tim-hub <tim-hub@users.noreply.github.com>
2024-09-09 21:19:51 +1200

    update logout to support both get and post

--- clients-side/frontend/prompt-smith-frontend/app/api/logout/route.ts
+++ clients-side/frontend/prompt-smith-frontend/app/api/logout/route.ts
@@ -1,6 +1,14 @@
 import { NextResponse } from 'next/server';
 import { deleteSession } from '@/lib/session';
 
+export async function GET() {
+  deleteSession();
+
+  return NextResponse.json({ message: 'Logged out successfully' });
+}
+
+
+
 export async function POST() {
   deleteSession();
 
--- clients-side/frontend/prompt-smith-frontend/app/api/simple-login/route.ts
+++ clients-side/frontend/prompt-smith-frontend/app/api/simple-login/route.ts
@@ -18,6 +18,27 @@ export async function POST(req: Request) {
     return NextResponse.json({errors: validatedFields.error.flatten().fieldErrors});
   }
 
+  // ping API protected health endpoint
+  const api_url = process.env.API_URL;
+  const url = `${api_url}/api/protected-ping`
+
+  try {
+    // to verify the bearer token is valid or not
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${bearerToken}`
+      }
+    });
+    if (response.status !== 200) {
+      return NextResponse.json({message: 'Failed to authenticate with API'});
+    }
+  } catch (error) {
+    console.error('Failed to authenticate', url, error);
+    return NextResponse.json({message: 'Failed to authenticate with API'});
+  }
+
+
   await createSimpleAuthSession(bearerToken);
   return NextResponse.json({message: 'Logged in successfully'});
 }
