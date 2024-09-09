5ee157969d0c23e0d52c39eccc50dc5cc02642c4
tim-hub <tim-hub@users.noreply.github.com>
2024-09-09 21:32:49 +1200

    add login page

--- clients-side/frontend/prompt-smith-frontend/app/login/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/login/page.tsx
@@ -1,13 +1,43 @@
-export default function LoginPage() {
+import {Button} from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+
+
+function LoginForm() {
   return (
-    <>
-      <form action="/api/simple-login" method="POST">
-        <div>
-          <label htmlFor="bearerToken">Token</label>
-          <input id="bearerToken" name="bearerToken" type="password"/>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  )
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Card className="w-full max-w-sm">
+          <form action="/api/simple-login" method="POST">
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your management token to login to your dashboard.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="bearerToken">Your Management Token</Label>
+                <Input id="bearerToken" type="password" name="bearerToken" required/>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" type="submit">Sign in</Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </main>
+    </div>
+)
 }
+
+export default LoginForm
