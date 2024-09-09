7899e37c4d38f6f22d2196b0fb9b8e1edeb45ea1
tim-hub <tim-hub@users.noreply.github.com>
2024-09-09 22:31:55 +1200

    update login page

--- clients-side/frontend/prompt-smith-frontend/app/login/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/login/page.tsx
@@ -14,7 +14,7 @@ import {Label} from "@/components/ui/label"
 function LoginForm() {
   return (
     <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Card className="w-full max-w-sm">
           <form action="/api/simple-login" method="POST">
