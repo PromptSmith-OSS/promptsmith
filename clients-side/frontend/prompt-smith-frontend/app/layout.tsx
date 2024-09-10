688170cee59d9b26259d26608a8b2296d7983cd8
tim-hub <tim-hub@users.noreply.github.com>
2024-09-10 21:00:54 +1200

    split layout

--- clients-side/frontend/prompt-smith-frontend/app/layout.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/layout.tsx
@@ -52,17 +52,8 @@ export default function RootLayout({
         <div className="flex flex-col">
           <Header/>
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-            <div className="flex items-center">
-              <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-            </div>
-            <div
-              className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-              x-chunk="dashboard-02-chunk-1"
-            >
-              <div className="flex flex-col items-center gap-1 text-center">
-                {children}
-              </div>
-            </div>
+            {children}
+
 
           </main>
         </div>
