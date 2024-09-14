f857454b8653306aabd3518e0b1048eb2bdd1212
tim-hub <tim-hub@users.noreply.github.com>
2024-09-14 21:33:28 +1200

    update layout

--- clients-side/frontend/prompt-smith-frontend/components/layout/dashboardLayout.tsx
+++ clients-side/frontend/prompt-smith-frontend/components/layout/dashboardLayout.tsx
@@ -15,12 +15,12 @@ const DashboardLayout = ({
         <SideBar/>
         <div className="flex flex-col">
           <Header/>
-          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 max-w-screen-lg w-full mx-auto">
             <div className="flex items-center">
               <h1 className="text-lg font-semibold md:text-2xl">{pageTitle}</h1>
             </div>
             <div
-              className="flex flex-1 justify-center rounded-lg border border-dashed shadow-sm p-4 md:p-8"
+              className="flex flex-1 justify-center rounded-lg border border-dashed shadow-sm p-4 md:p-8 "
             >
               {children}
             </div>
