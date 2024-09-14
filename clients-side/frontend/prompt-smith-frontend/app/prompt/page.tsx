c4b5af946959f9768e7d95400ed4e8d20fcfbf59
tim-hub <tim-hub@users.noreply.github.com>
2024-09-14 21:30:15 +1200

    update prompt page UI

--- clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
@@ -10,50 +10,6 @@ import {
 } from "@/components/ui/table"
 import {getBearerTokenFromSession} from "@/lib/session";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-]
 
 const getAllPrompts = async () => {
   const token = await getBearerTokenFromSession();
@@ -64,48 +20,51 @@ const getAllPrompts = async () => {
     }
   })
   const data = await response.json()
-  return data
+  return data.items
 }
 
-export async function TableDemo() {
+export async function Page() {
   const prompts = await getAllPrompts()
 
   return (
     <div className='w-full'>
-      <pre>
-        <code>
-          {JSON.stringify(prompts, null, 2)}
-        </code>
-      </pre>
+      {/*<pre>*/}
+      {/*  <code>*/}
+      {/*    {JSON.stringify(prompts, null, 2)}*/}
+      {/*  </code>*/}
+      {/*</pre>*/}
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
+            <TableHead className="w-[100px]">Key</TableHead>
             <TableHead>Method</TableHead>
             <TableHead className="text-right">Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          {prompts?.map((prompt) => (
+            <TableRow key={prompt.unique_key}>
+              <TableCell className="font-medium">
+                {prompt.unique_key}
+                <br/>
+                <span className="text-xs text-gray-500">{prompt.description}</span>
+              </TableCell>
+              <TableCell></TableCell>
+              {/*<TableCell>{invoice.paymentMethod}</TableCell>*/}
+              {/*<TableCell className="text-right">{invoice.totalAmount}</TableCell>*/}
             </TableRow>
           ))}
         </TableBody>
         <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
+          {/*<TableRow>*/}
+          {/*  <TableCell colSpan={3}>Total</TableCell>*/}
+          {/*  <TableCell className="text-right">$2,500.00</TableCell>*/}
+          {/*</TableRow>*/}
         </TableFooter>
       </Table>
     </div>
   )
 }
 
-export default TableDemo
+export default Page
--- clients-side/frontend/prompt-smith-frontend/components/layout/dashboardLayout.tsx
+++ clients-side/frontend/prompt-smith-frontend/components/layout/dashboardLayout.tsx
@@ -20,7 +20,7 @@ const DashboardLayout = ({
               <h1 className="text-lg font-semibold md:text-2xl">{pageTitle}</h1>
             </div>
             <div
-              className="flex flex-1 justify-center rounded-lg border border-dashed shadow-sm p-4"
+              className="flex flex-1 justify-center rounded-lg border border-dashed shadow-sm p-4 md:p-8"
             >
               {children}
             </div>
