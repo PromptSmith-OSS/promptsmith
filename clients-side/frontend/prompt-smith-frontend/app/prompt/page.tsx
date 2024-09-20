c4771ad8a28fe568d1c89ff4ec0962a138c443e3
tim-hub <tim-hub@users.noreply.github.com>
2024-09-20 18:34:13 +1200

    make prompt fetch all page work as server component

--- clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
@@ -8,18 +8,62 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import {getBearerTokenFromSession} from "@/lib/session";
-import Prompt from "@/components/features/prompt";
 
-
-
+const getAllPrompts = async () => {
+  const selfUrl = process.env.NEXT_PUBLIC_BASE_URL
+  const response = await fetch(selfUrl+'/api/bff/api/prompt', {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  console.log(response)
+  const data = await response.json()
+  return data.items
+}
 
 export async function Page() {
-
+  const prompts = await getAllPrompts()
 
   return (
-    <Prompt
-      />
+    <div className='w-full'>
+      {/*<pre>*/}
+      {/*  <code>*/}
+      {/*    {JSON.stringify(prompts, null, 2)}*/}
+      {/*  </code>*/}
+      {/*</pre>*/}
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Key</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {prompts?.map((prompt) => (
+            <TableRow key={prompt.unique_key}>
+              <TableCell className="font-medium">
+                {prompt.unique_key}
+                <br/>
+                <span className="text-xs text-gray-500">{prompt.description}</span>
+              </TableCell>
+              <TableCell></TableCell>
+              {/*<TableCell>{invoice.paymentMethod}</TableCell>*/}
+              {/*<TableCell className="text-right">{invoice.totalAmount}</TableCell>*/}
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          {/*<TableRow>*/}
+          {/*  <TableCell colSpan={3}>Total</TableCell>*/}
+          {/*  <TableCell className="text-right">$2,500.00</TableCell>*/}
+          {/*</TableRow>*/}
+        </TableFooter>
+      </Table>
+    </div>
   )
 }
 
