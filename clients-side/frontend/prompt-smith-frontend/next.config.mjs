cd44c097449e5c64d4c47c0684c3e736e5943b66
tim-hub <tim-hub@users.noreply.github.com>
2024-09-20 18:32:04 +1200

    use rewrite as proxy

--- clients-side/frontend/prompt-smith-frontend/next.config.mjs
+++ clients-side/frontend/prompt-smith-frontend/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const api_url = process.env.API_URL;
+
+const nextConfig = {
+
+    /**
+     * Use rewrites to proxy requests to the API server
+     * API server will not consider this as CORS request but same region request
+     * @returns {Promise<[{destination: string, source: string}]>}
+     */
+    async rewrites() {
+        return [
+{
+    source: "/api/bff/:path*",
+    destination: api_url + "/:path*/",
+},
+{
+    source: "/api/bff/:path*/",
+    destination: api_url + "/:path*/",
+},
+        ];
+    },
+};
 
 export default nextConfig;
