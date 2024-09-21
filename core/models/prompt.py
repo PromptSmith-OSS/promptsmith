174ab0fd9edd725aaf95c99d11a2e062cfc4247a
tim-hub <tim-hub@users.noreply.github.com>
2024-09-21 16:25:36 +1200

    add project relation

--- core/models/prompt.py
+++ core/models/prompt.py
@@ -7,7 +7,7 @@ class Prompt(UUIDBasedBaseModel):
     unique_key = models.CharField(max_length=256, unique=True, editable=True)
     description = models.TextField(max_length=2048)
     enabled = models.BooleanField(default=True)
-    # project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.text
