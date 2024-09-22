2492d0256a0d14a268c0af77a06ab60af0d0ba8b
tim-hub <tim-hub@users.noreply.github.com>
2024-09-22 17:59:04 +1200

    better naming

--- core/auth.py
+++ core/auth.py
@@ -1,18 +1,14 @@
 from typing import Optional, Any
 
 from django.http import HttpRequest
-from django.utils.decorators import method_decorator
+from django.shortcuts import aget_object_or_404
+from ninja.errors import ValidationError
 
 from project.models import Project
 from shared.auth import AsyncDjangoNinjaAuth
-from ninja.errors import AuthenticationError, ValidationError
-
-from asgiref.sync import sync_to_async
-
-from django.shortcuts import aget_object_or_404
 
 
-class AsyncCoreResourceAuth(AsyncDjangoNinjaAuth):
+class AsyncCoreResourceAuthenticationAndAuthorization(AsyncDjangoNinjaAuth):
     """
     Reusing Django session authentication
     This decouple the authentication from Ninja routers and Django All Auth
@@ -49,4 +45,4 @@ class AsyncCoreResourceAuth(AsyncDjangoNinjaAuth):
         return project
 
 
-async_core_resource_auth = AsyncCoreResourceAuth()
+async_core_resource_auth = AsyncCoreResourceAuthenticationAndAuthorization()
