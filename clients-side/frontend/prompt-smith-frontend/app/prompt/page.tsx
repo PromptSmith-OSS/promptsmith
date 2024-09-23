7cae717423960b7e117758940db7247e730f6652
tim-hub <tim-hub@users.noreply.github.com>
2024-09-23 21:14:44 +1200

    use client prompt to make it work

--- clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
@@ -9,6 +9,7 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import {Prompt} from "@/lib/interfaces";
+import PromptPage from "@/components/features/prompt";
 
 const getAllPrompts = async ():Promise<Prompt[]> => {
   const selfUrl = process.env.NEXT_PUBLIC_BASE_URL
@@ -36,36 +37,37 @@ export async function Page() {
       {/*    {JSON.stringify(prompts, null, 2)}*/}
       {/*  </code>*/}
       {/*</pre>*/}
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Key</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {prompts?.map((prompt) => (
-            <TableRow key={prompt.unique_key}>
-              <TableCell className="font-medium">
-                {prompt.unique_key}
-                <br/>
-                <span className="text-xs text-gray-500">{prompt.description}</span>
-              </TableCell>
-              <TableCell></TableCell>
-              {/*<TableCell>{invoice.paymentMethod}</TableCell>*/}
-              {/*<TableCell className="text-right">{invoice.totalAmount}</TableCell>*/}
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          {/*<TableRow>*/}
-          {/*  <TableCell colSpan={3}>Total</TableCell>*/}
-          {/*  <TableCell className="text-right">$2,500.00</TableCell>*/}
-          {/*</TableRow>*/}
-        </TableFooter>
-      </Table>
+      {/*<Table>*/}
+      {/*  <TableCaption>A list of your recent invoices.</TableCaption>*/}
+      {/*  <TableHeader>*/}
+      {/*    <TableRow>*/}
+      {/*      <TableHead className="w-[100px]">Key</TableHead>*/}
+      {/*      <TableHead>Method</TableHead>*/}
+      {/*      <TableHead className="text-right">Amount</TableHead>*/}
+      {/*    </TableRow>*/}
+      {/*  </TableHeader>*/}
+      {/*  <TableBody>*/}
+      {/*    {prompts?.map((prompt) => (*/}
+      {/*      <TableRow key={prompt.unique_key}>*/}
+      {/*        <TableCell className="font-medium">*/}
+      {/*          {prompt.unique_key}*/}
+      {/*          <br/>*/}
+      {/*          <span className="text-xs text-gray-500">{prompt.description}</span>*/}
+      {/*        </TableCell>*/}
+      {/*        <TableCell></TableCell>*/}
+      {/*        /!*<TableCell>{invoice.paymentMethod}</TableCell>*!/*/}
+      {/*        /!*<TableCell className="text-right">{invoice.totalAmount}</TableCell>*!/*/}
+      {/*      </TableRow>*/}
+      {/*    ))}*/}
+      {/*  </TableBody>*/}
+      {/*  <TableFooter>*/}
+      {/*    /!*<TableRow>*!/*/}
+      {/*    /!*  <TableCell colSpan={3}>Total</TableCell>*!/*/}
+      {/*    /!*  <TableCell className="text-right">$2,500.00</TableCell>*!/*/}
+      {/*    /!*</TableRow>*!/*/}
+      {/*  </TableFooter>*/}
+      {/*</Table>*/}
+      <PromptPage />
     </div>
   )
 }
