e910517a5a5b9d3b9cd64af8161e580e07b47843
tim-hub <tim-hub@users.noreply.github.com>
2024-09-23 22:00:16 +1200

    add create and navigation to create page

--- clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
+++ clients-side/frontend/prompt-smith-frontend/app/prompt/page.tsx
@@ -1,5 +1,6 @@
-import PromptPage from "@/components/features/prompt";
+import PromptTable from "@/components/features/prompt";
 import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 
 export async function Page() {
@@ -7,11 +8,13 @@ export async function Page() {
     <div className='w-full'>
       <div className="flex flex-row w-full justify-between pb-2 mb-2">
         <div></div>
-        <Button variant="default" size="default" className="flex items-center justify-center">
-          Create Prompt
+        <Button variant="default" size="default" className="flex items-center justify-center" asChild>
+          <Link href={'/prompt/create'}>
+            Create Prompt
+          </Link>
         </Button>
       </div>
-      <PromptPage/>
+      <PromptTable/>
     </div>
   )
 }
