7d2fb6b1a2ffcf3c3defd0d448cd693d55fb56fd
tim-hub <tim-hub@users.noreply.github.com>
2024-09-30 23:02:07 +1300

    fix variant create response validation error

--- core/routers/prompt_variant.py
+++ core/routers/prompt_variant.py
@@ -16,7 +16,7 @@ prompt_variant_router = Router(
 )
 
 
-@prompt_variant_router.post('/{prompt_uuid}/variant/', response=PromptVariantOutSchema)
+@prompt_variant_router.post('/{prompt_uuid}/variant', response=PromptVariantOutSchema)
 async def create_prompt_variant(request, prompt_uuid: UUID, variant: PromptVariantCreateSchema):
     """
     Create a new prompt variant
@@ -29,20 +29,24 @@ async def create_prompt_variant(request, prompt_uuid: UUID, variant: PromptVaria
 
     # check if the name is unique
     if await PromptVariant.objects.filter(prompt=the_prompt, name=variant.name).aexists():
-        raise ValidationError([{'type': 'duplication', 'name': version.name, 'msg': f'Same name has already existed.'}])
+        raise ValidationError([{'type': 'duplication', 'name': variant.name, 'msg': f'Same name has already existed.'}])
 
     new_variant = await PromptVariant.objects.acreate(prompt=the_prompt, **variant.dict())
-    new_variant.prompt_uuid = the_prompt.uuid
-    return new_variant
+    qs = PromptVariant.objects.filter(uuid=new_variant.uuid).prefetch_related('versions')
+    return await aget_object_or_404(qs)
 
 
-@prompt_variant_router.get('/{prompt_uuid}/variant/', response=List[PromptVariantOutSchema])
+@prompt_variant_router.get('/{prompt_uuid}/variant', response=List[PromptVariantOutSchema])
 async def get_all_prompt_variants(request, prompt_uuid: UUID):
     """
     Get all variants for a prompt
     """
     # django queryset cannot be used to return as List[PrompVariantOutSchema] directly
-    qs = PromptVariant.objects.filter(prompt__uuid=prompt_uuid).all().select_related('prompt', 'segment').annotate(
+    qs = PromptVariant.objects.filter(prompt__uuid=prompt_uuid).all().select_related(
+        'prompt', 'segment'
+    ).prefetch_related(
+        'versions'
+    ).annotate(
         prompt_uuid=models.F('prompt__uuid'),
         segment_uuid=models.F('segment__uuid'),
     )
