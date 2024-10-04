ee0668a83d1a28905fbcc2f37234b375383ac829
tim-hub <tim-hub@users.noreply.github.com>
2024-10-04 21:39:10 +1300

    hide quick navigation for now

--- clients-side/frontend/prompt-smith-frontend/components/layout/header.tsx
+++ clients-side/frontend/prompt-smith-frontend/components/layout/header.tsx
@@ -22,16 +22,16 @@ const Header = () => {
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <SidebarMobile/>
       <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"/>
-            <Input
-              type="search"
-              placeholder="Quick navigation"
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
+        {/*<form>*/}
+        {/*  <div className="relative">*/}
+        {/*    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"/>*/}
+        {/*    <Input*/}
+        {/*      type="search"*/}
+        {/*      placeholder="Quick navigation"*/}
+        {/*      className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*</form>*/}
       </div>
       <ModeToggle/>
       {/*<Button variant="outline" size="icon" className="ml-auto h-9 w-9">*/}
