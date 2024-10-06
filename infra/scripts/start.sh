04e92b9903f7eb324f361561aa4edc9b87e88bca
tim-hub <tim-hub@users.noreply.github.com>
2024-10-06 21:19:32 +1300

    add docker file for django part of backend

--- infra/scripts/start.sh
+++ infra/scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run migrations, collect static files and start server
-export DJANGO_SETTINGS_MODULE=PromptSmith.settings_production
+export DJANGO_SETTINGS_MODULE=promptsmith.settings_production
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 #poetry run python manage.py runserver
-gunicorn --env PromptSmith.wsgi:application --bind 0.0.0.0:8000 --workers 2 --log-level=info
+gunicorn --env promptsmith.wsgi:application --bind 0.0.0.0:8000 --workers 2 --log-level=info
